3dec5a50c7f05038e916bf2cfada97f1970169e5
Vikas Dayananda <Vikas.Dayananda@aexp.com>
2019-10-13 00:27:09 -0700

    Updates

---
+++ precache-manifest.4996689b80ae757b0be9e81d5bd34bfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1284a0551189b4ef8c70add417d8b484",
+    "url": "/VikasDayananda/tune-app/index.html"
+  },
+  {
+    "revision": "7ebbf91b707979b16181",
+    "url": "/VikasDayananda/tune-app/static/css/main.b736f213.chunk.css"
+  },
+  {
+    "revision": "b0a67da89146891ad33f",
+    "url": "/VikasDayananda/tune-app/static/js/2.dace8bc7.chunk.js"
+  },
+  {
+    "revision": "7ebbf91b707979b16181",
+    "url": "/VikasDayananda/tune-app/static/js/main.3e61610e.chunk.js"
+  },
+  {
+    "revision": "59b404ab55c3d0f4ebc5",
+    "url": "/VikasDayananda/tune-app/static/js/runtime-main.d96e0166.js"
+  }
+]);
